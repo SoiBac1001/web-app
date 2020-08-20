a5e66d0ff47554b28a8b00465253dfa9470e1b07
SoiBac1001 <congheit196@gmail.com>
2020-08-20 08:12:47 +0700

    implement code BE chat web socket

--- src/main/java/com/webapp/webdemo/listener/WebSocketEventListener.java
+++ src/main/java/com/webapp/webdemo/listener/WebSocketEventListener.java
@@ -1,13 +1,15 @@
 package com.webapp.webdemo.listener;
 
+import com.webapp.webdemo.constants.enums.MessageType;
+import com.webapp.webdemo.payload.request.chat.ChatMessage;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @AllArgsConstructor
 @Slf4j
@@ -22,7 +24,15 @@ public class WebSocketEventListener {
     }
 
     @EventListener
-    public void handleWebSocketDisconnectListener(){
-        StomHea
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if(username != null){
+            log.info("User disconnected: " + username);
+            ChatMessage chatMessage = new ChatMessage();
+            chatMessage.setType(MessageType.LEAVE);
+            chatMessage.setSender(username);
+            messageSendingOperations.convertAndSend("/topic/public", chatMessage);
+        }
     }
 }
